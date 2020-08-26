999d4e0c240b46243d37a1f060e8932ef6e6c29a
Lasse Claes Jacobsen <lacj@Lasses-MacBook-Pro.local>
2020-08-26 17:05:23 +0200

    script fixed

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 window.addEventListener("load", () => {
-  setupCircles();
+  setupCircles2();
 })
 
-function setupCircles() {
+//first
+//function setupCircles() {
+//  const cs = document.querySelectorAll('div');
+//  cs.forEach(el => {
+//    //    console.log(`$ {
+//    //      hi
+//    //    }`);
+//    el.style.transform = `translate(${
+//            Math.random() * 100
+//          }vw, ${
+//            Math.random() * 100
+//          }vh)`;
+//    el.addEventListener("mouseover", c => {
+//      el.style.background =
+//        `hsl(${Math.random()*360}, 70%, 70%)`;
+//      el.style.transform = `translate(${
+//            Math.random() * 100
+//          }vw, ${
+//            Math.random() * 100
+//          }vh)`;
+//    })
+//  })
+//}
+
+function transform(elem) {
+  elem.style.transform = `translate(${
+            Math.random() * 100
+          }vw, ${
+            Math.random() * 100
+          }vh)`;
+}
+
+function setupCircles2() {
   const cs = document.querySelectorAll('div');
   cs.forEach(el => {
     //    console.log(`$ {
     //      hi
     //    }`);
-    el.style.transform = `translate(${
-            Math.random() * 100
-          }vw, ${
-            Math.random() * 100
-          }vh)`;
+    transform(el);
+
+    el.addEventListener("mouseover", c => {
+      el.style.background =
+        `hsl(${Math.random()*360}, 70%, 70%)`;
+      transform(el);
+    })
   })
 }
